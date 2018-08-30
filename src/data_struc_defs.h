a7089aadf655c2cdc10adac648db32a54ba30ba1
Alejandro Aviles <aviles@ciencias.unam.mx>
2018-08-30 10:17:04 -0500

    Update data_struc_defs.h

--- src/data_struc_defs.h
+++ src/data_struc_defs.h
@@ -1,26 +1,5 @@
 /*==============================================================================
  HEADER: data_struc_defs.h		[mglpt]
- Written by: Mario A. Rodriguez-Meza
- Starting date: January 2018
- Purpose: Definitions of global variables and parameters
- Language: C
- Use: '#include "global_defs.h"
- Use in routines and functions:
- External headers: stdinc.h, data_struc_defs.h
- Comments and notes:
- Info: M.A. Rodriguez-Meza
- Depto. de Fisica, ININ
- Apdo. Postal 18-1027 Mexico D.F. 11801 Mexico
- e-mail: marioalberto.rodriguez@inin.gob.mx
- http://www.inin.gob.mx/
- 
- Major revisions:
- Copyright: (c) 2005-2018 Mar.  All Rights Reserved
- ================================================================================
- Legal matters:
- The author does not warrant that the program and routines it contains
- listed below are free from error or suitable for particular applications,
- and he disclaims all liability from any consequences arising from their	use.
  ==============================================================================*/
 
  
