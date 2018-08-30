5ae1ff2a5362b9408a3df4c4388cad182c7faf99
Alejandro Aviles <aviles@ciencias.unam.mx>
2018-08-30 10:26:42 -0500

    Update getparam.h

--- src/general_libs/getparam.h
+++ src/general_libs/getparam.h
@@ -1,26 +1,5 @@
 /*==============================================================================
 	HEADER: getparam.h			[General_libs]
-	Written by: M.A. Rodriguez-Meza
-	Starting date: May 2006
-	Purpose: definitions and protodefinitions to initialize the parameters code
-	Language: C
-	Use: '#include "getparam.h"'
-	Use in routines and functions:
-	External headers:
-	Coments and notes:
-	Info: M.A. Rodriguez-Meza
-		Depto. de Fisica, ININ
-		Apdo. Postal 18-1027 Mexico D.F. 11801 Mexico
-		e-mail: marioalberto.rodriguez@inin.gob.mx
-		http://www.astro.inin.mx/mar
-
-	Major revisions: January 2007;
-	Copyright: (c) 2005-2011 Mar.  All Rights Reserved
-================================================================================
-	Legal matters:
-	The author does not warrant that the program and routines it contains
-	listed below are free from error or suitable for particular applications,
-	and he disclaims all liability from any consequences arising from their use.
 ==============================================================================*/
 
 
