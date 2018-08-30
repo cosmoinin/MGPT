b76badb557b2cc1947f3b8d83de811e4043d6dd4
Alejandro Aviles <aviles@ciencias.unam.mx>
2018-08-30 10:25:53 -0500

    Update constant.h

--- src/general_libs/constant.h
+++ src/general_libs/constant.h
@@ -1,26 +1,5 @@
 /* ==============================================================================
 	HEADER: constant.h
-	Written by: M.A. Rodriguez-Meza
-	Starting date: February 2000
-	Purpose: Definitions of global constant and parameters
-	Language: C
-	Use: '#include "...."
-	Use in routines and functions:
-	External headers:
-	Comments and notes:
-	Info: M.A. Rodriguez-Meza
-		Depto. de Fisica, ININ
-		Apdo. Postal 18-1027 Mexico D.F. 11801 Mexico
-		e-mail: marioalberto.rodriguez@inin.gob.mx
-		http://www.astro.inin.mx/mar
-
-	Major revisions:
-	Copyright: (c) 2005-2010 Mar.  All Rights Reserved
-================================================================================
-	Legal matters:
-	The author does not warrant that the program and routines it contains
-	listed below are free from error or suitable for particular applications,
-	and he disclaims all liability from any consequences arising from their	use.
 ==============================================================================*/
 
 #ifndef _constants_h_
