6ec1ad559f1bebed92fff5bed4726cdddf089a8e
cosmoinin <42773394+cosmoinin@users.noreply.github.com>
2018-08-31 08:39:07 -0500

    Update mglpt.c

--- src/mglpt.c
+++ src/mglpt.c
@@ -78,7 +78,7 @@ local void computingQRs(void)
         ki = rpow(10.0,kval);
         fprintf(stdout,"i: %d :: ki: %g :: ",i,ki);
         fflush(stdout);
-        qrs = QsR1R2_functions_driver(gd.xstop, ki);
+        qrs = QsRs_functions_driver(gd.xstop, ki);
 
         Q1 = qrs.Q1;
         Q2 = qrs.Q2;
