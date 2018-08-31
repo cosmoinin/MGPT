dc53dd1b2dd1efb3506764be66af88e5ca104b6d
cosmoinin <42773394+cosmoinin@users.noreply.github.com>
2018-08-31 08:12:55 -0500

    Update protodefs.h

--- src/protodefs.h
+++ src/protodefs.h
@@ -20,7 +20,6 @@ global void biasterms_processing(void);
 
 
 real psLCDMf(real k);       // Interpolation of the power spectrum
-global  real psInterpolation(real k, pointPSTableptr PSLtab, int nPSL);
 global  real psInterpolation_nr(real k, double kPS[], double pPS[], int nPS);
 
 // MGLPT DIFFEQS
@@ -36,25 +35,9 @@ global global_D3v2_ptr DsThirdOrder_func_ver2(real x, real k, real p);
 
 // MGLPT QUADS
 global_QRs QsR1R2_functions_driver(real eta, real ki);
-global real GaussLegendreQ1_func_ver3(real y);
-global real GaussLegendreQ2_func_ver3(real y);
-global real GaussLegendreQ3_func_ver3(real y);
-global real GaussLegendreQ8_func_ver3(real y);
-global real GaussLegendreQ9_func_ver3(real y);
-global real GaussLegendreQ13_func_ver3(real y);
-global real GaussLegendreQI_func_ver3(real y);
-global real GaussLegendreQ5_func_ver3(real y);
-global real GaussLegendreQ7_func_ver3(real y);
-global real GaussLegendreQ11_func_ver3(real y);
-global real GaussLegendreQ12_func_ver3(real y);
-global real GaussLegendreRI_func_ver3(real y);
-global real GaussLegendreR1p2_func_ver3(real y);
-global real GaussLegendreR1_func_ver3(real y);
-global real GaussLegendreR2_func_ver3(real y);
-global real funcR1int_ver4(real y);
 
 
-// functions (mglpt_fns)
+// functions (mgpt_fns)
 real OmM(real eta);
 real H(real eta);
 real f1(real eta);
